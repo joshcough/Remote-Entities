4c4d8c17449a7b1eca495f5b85bfdcf203a5559b
joshcough <joshcough@gmail.com>
2014-05-16 14:18:27 -0400

    added build status image

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/joshcough/Remote-Entities.png?branch=master)](https://travis-ci.org/joshcough/Remote-Entities)
+
 Remote-Entities
 ===============
 
---
+++ src/main/java/com/joshcough/remoteentities/api/features/features/Feature.scala
@@ -0,0 +1,37 @@
+package com.joshcough.remoteentities.api.features
+
+import de.kumpelblase2.remoteentities.api.RemoteEntity
+import com.joshcough.remoteentities.persistence.SerializableData
+
+trait Feature extends SerializableData {
+  /**
+   * Gets the name of the feature
+   *
+   * @return name
+   */
+  def getName: String
+
+  /**
+   * Gets the entity that owns this feature
+   *
+   * @return entity
+   */
+  def getEntity: RemoteEntity
+
+  /**
+   * Method that gets executed when the feature gets added to the entity
+   */
+  def onAdd
+
+  /**
+   * Method that gets executed when the feature gets added to the given entity
+   *
+   * @param inEntity  Entity the feature gets added to
+   */
+  def onAdd(inEntity: RemoteEntity)
+
+  /**
+   * Method that gets executed when the feature gets removed from the entity
+   */
+  def onRemove
+}
